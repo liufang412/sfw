0802f193fa24e68fa535df0f734bf3872d95b187
unknown <1227681877@qq.com>
2016-07-07 10:51:07 +0800

    a

---
+++ js/home.js
@@ -0,0 +1,175 @@
+$(function(){
+	$('.city').mouseover(function(){
+		$('.city div').css({'display':'block'});
+	});
+	$('.city').mouseout(function(){
+		$('.city div').css({'display':''});
+	});
+	$('.new').mouseover(function(){
+		$('.new div').css({'display':'block'});
+	});
+	$('.new').mouseout(function(){
+		$('.new div').css({'display':''});
+	});
+	$('.twice').mouseover(function(){
+		$('.twice div').css({'display':'block'});
+	});
+	$('.twice').mouseout(function(){
+		$('.twice div').css({'display':''});
+	});
+	$('.rent').mouseover(function(){
+		$('.rent div').css({'display':'block'});
+	});
+	$('.rent').mouseout(function(){
+		$('.rent div').css({'display':''});
+	});
+	$('.decorate').mouseover(function(){
+		$('.decorate div').css({'display':'block'});
+	});
+	$('.decorate').mouseout(function(){
+		$('.decorate div').css({'display':''});
+	});
+	$('.write').mouseover(function(){
+		$('.write div').css({'display':'block'});
+	});
+	$('.write').mouseout(function(){
+		$('.write div').css({'display':''});
+	});
+	$('.business').mouseover(function(){
+		$('.business div').css({'display':'block'});
+	});
+	$('.business').mouseout(function(){
+		$('.business div').css({'display':''});
+	});
+	$('.business2').mouseover(function(){
+		$('.business2 div').css({'display':'block'});
+	});
+	$('.business2').mouseout(function(){
+		$('.business2 div').css({'display':''});
+	});
+	$('.industry').mouseover(function(){
+		$('.industry div').css({'display':'block'});
+	});
+	$('.industry').mouseout(function(){
+		$('.industry div').css({'display':''});
+	});
+	$('.overseas').mouseover(function(){
+		$('.overseas div').css({'display':'block'});
+	});
+	$('.overseas').mouseout(function(){
+		$('.overseas div').css({'display':''});
+	});
+	$('.owner').mouseover(function(){
+		$('.owner div').css({'display':'block'});
+	});
+	$('.owner').mouseout(function(){
+		$('.owner div').css({'display':''});
+	});
+	$('.news').mouseover(function(){
+		$('.news div').css({'display':'block'});
+	});
+	$('.news').mouseout(function(){
+		$('.news div').css({'display':''});
+	});
+	$('.app').mouseover(function(){
+		$('.app div').css({'display':'block'});
+	});
+	$('.app').mouseout(function(){
+		$('.app div').css({'display':''});
+	});
+	$('.dl').mouseover(function(){
+		$('.header-login .login').css({'display':'block'});
+	});
+	$('.dl').mouseout(function(){
+		$('.header-login .login').css({'display':''});
+	});
+	$('.login').mouseover(function(){
+		$(' .login').css({'display':'block'});
+	});
+	$('.login').mouseout(function(){
+		$('.login').css({'display':''});
+	});
+	$('.nav-right').mouseover(function(){
+		$(' .more').css({'display':'block'});
+	});
+	$('.nav-right').mouseout(function(){
+		$('.more').css({'display':''});
+	});
+	$('.more').mouseover(function(){
+		$(' .more').css({'display':'block'});
+	});
+	$('.more').mouseout(function(){
+		$('.more').css({'display':''});
+	});
+	$('.nav-left li:eq(0)').mouseover(function(){
+			$('.search input').attr({'placeholder':'请输入小区名称或地址，给自己的房子估个价'});
+	});
+	$('.nav-left li:eq(1)').mouseover(function(){
+			$('.search input').attr({'placeholder':'请输入关键字（楼盘名/地名/开发商等)'});
+	});
+	$('.nav-left li:eq(2)').mouseover(function(){
+			$('.search input').attr({'placeholder':'请输入关键字（楼盘名或地名)'});
+	});
+	$('.nav-left li:eq(3)').mouseover(function(){
+			$('.search input').attr({'placeholder':'请输入关键字（楼盘名或地名)'});
+	});
+	$('.nav-left li:eq(4)').mouseover(function(){
+			$('.search input').attr({'placeholder':'请输入户型/功能间/风格等关键词'});
+	});
+	$('.nav-left li:eq(5)').mouseover(function(){
+			$('.search input').attr({'placeholder':'请输入关键字(国家/城市/地区)'});
+	});
+	
+	$('.activity li:eq(0)').mouseover(function(){
+		$('.yli').css({'display':'none'});
+		$('.watch').css({'display':'none'});
+		$('.dzhe').css({'display':'block'});
+	});
+	$('.activity li:eq(1)').mouseover(function(){
+		$('.dzhe').css({'display':'none'});
+		$('.watch').css({'display':'none'});
+		$('.yli').css({'display':'block'});
+	});
+	$('.activity li:eq(2)').mouseover(function(){
+		$('.dzhe').css({'display':'none'});
+		$('.yli').css({'display':'none'});
+		$('.watch').css({'display':'block'});
+	});
+	$('.laybox-content1-top a:eq(0)').mouseover(function(){
+		$(' .ul1').css({'display':'block'});
+		$(' .ul2').css({'display':'none'});
+
+	});
+	$('.laybox-content1-top a:eq(1)').mouseover(function(){
+		$('.ul2').css({'display':'block'});
+		$('.ul1').css({'display':'none'});
+	});
+	$('.grey').mouseover(function(){
+		$('.grey-div').css({'display':'block'});
+		
+	});
+	$('.grey').mouseout(function(){
+		$('.grey-div').css({'display':''});
+		
+	});
+	$('.grey-div').mouseover(function(){
+		$('.grey-div').css({'display':'block'});
+		$(".gry").css({'background-position':'0px -180px'});
+		
+	});
+	$('.grey-div').mouseout(function(){
+		$('.grey-div').css({'display':''});
+		$(".gry").css({'background-position':'0px 0px'});
+		
+	});
+	
+
+		
+		
+	
+
+	
+
+
+	
+})
